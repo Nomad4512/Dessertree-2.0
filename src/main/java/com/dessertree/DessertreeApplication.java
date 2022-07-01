ea0125966cdc7b0bd46eb23ce376d4399fd017f0
Nomad4512 <zizonbini@gmail.com>
2022-07-01 16:42:01 +0900

    first commit

---
+++ src/main/java/com/dessertree/DessertreeApplication.java
@@ -0,0 +1,13 @@
+package com.dessertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dessertree/DessertreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dessertree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
