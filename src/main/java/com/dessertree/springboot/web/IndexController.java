e847f236699dbf70c4f4713dac10c1cdb931d372
hyunbin <zizonbini@gmail.com>
2023-02-14 01:33:20 +0900

    index페이지 헤더,바디,푸터 나누고 테스트 완료

--- src/main/java/com/dessertree/springboot/web/IndexController.java
+++ src/main/java/com/dessertree/springboot/web/IndexController.java
@@ -1,14 +1,18 @@
 package com.dessertree.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequiredArgsConstructor
+@Controller
 public class IndexController {
 
-    @GetMapping("/hello") // 테스트
-    public static String hello(){
+    @GetMapping("/")
+    public String index() {
 
-        return "hello";
+        return "index";
     }
 }
